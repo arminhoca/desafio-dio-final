8928e17df60c5dfba428582c11099d9d5d665469
arminhoca <165800103+arminhoca@users.noreply.github.com>
2024-07-28 12:11:28 -0300

    Update GlobalExceptionHandler.java

--- src/main/java/me/dio/controller/exception/GlobalExceptionHandler.java
+++ src/main/java/me/dio/controller/exception/GlobalExceptionHandler.java
@@ -12,7 +12,7 @@ import java.util.NoSuchElementException;
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
-    private final Logger logger  = LoggerFactory.getLogger(GlobalExceptionHandler.class);
+    private static final Logger logger  = LoggerFactory.getLogger(GlobalExceptionHandler.class);
 
     @ExceptionHandler(IllegalArgumentException.class)
     public ResponseEntity<String> handleBusinessException(IllegalArgumentException businessException) {
